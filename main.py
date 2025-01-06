0802c251c707f4d3b688d1ac6f8b2c6473aa047b
alexrowe99 <arowe890@gmail.com>
2025-01-06 12:15:00 +1030

    multiple updates

--- main.py
+++ main.py
@@ -6,34 +6,41 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    dt = 0
-    AsteroidField.containers = (updatable)
-    Asteroid.containers = (updatable, drawable, asteroids)
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    field = AsteroidField()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        for entity in updatable:
-            entity.update(dt)
-        for sprite in drawable:
-            sprite.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	dt = 0
+	AsteroidField.containers = (updatable)
+	Asteroid.containers = (updatable, drawable, asteroids)
+	Player.containers = (updatable, drawable)
+	Shot.containers = (updatable, drawable, shots)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	field = AsteroidField()
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		for entity in updatable:
+			entity.update(dt)
+		for asteroid in asteroids:
+			if asteroid.hasCollided(player):
+				print("Game over!")
+				return
+		for sprite in drawable:
+			sprite.draw(screen)
+		pygame.display.flip()
+		dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
-    main()
+	main()
